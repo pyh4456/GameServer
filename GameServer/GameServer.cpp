f748f4dacd1017a493a9dee126e6a86262d2f451
pyh4456 <pyh4456>
2023-09-07 18:56:24 +0900

    memory pool(LockFreeStack)

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -11,43 +11,53 @@
 #include "Allocator.h"
 #include "LockFreeStack.h"
 
-class Player {
+DECLSPEC_ALIGN(16)
+class Data
+{
 public:
-	Player() {}
-	virtual ~Player() {	}
-};
+	SListEntry _entry;
 
-class Knight : public Player {
-public:
-	Knight() {
-		cout << "Knight()" << endl;
-	}
-	~Knight() {
-		cout << "~Knight()" << endl;
-	}
-
-	int32 _hp = 100;
-	int32 _mp = 10;
-
+	int64 _rand = rand() % 1000;
 };
 
 
+SListHeader* GHeader;
 
 int main() {
+	GHeader = new SListHeader();
+	ASSERT_CRASH(((uint64)GHeader % 16) == 0);
 
-	for (int32 i = 0; i < 5; i++) {
+	InitializeHead(GHeader);
+
+	for (int32 i = 0; i < 3; i++) {
 		GThreadManager->Launch([]()
 			{
 				while (true) {
-					Vector<Knight> v(10);
+					Data* data = new Data();
+					ASSERT_CRASH(((uint64)GHeader % 16) == 0);
 
-					Map<int32, Knight> m;
-					m[100] = Knight();
-
+					PushEntrySList(GHeader, (SListEntry*)data);
 					this_thread::sleep_for(10ms);
 				}
-			}
-		);
+			});
+	}
+
+	for (int32 i = 0; i < 2; i++) {
+		GThreadManager->Launch([]()
+			{
+				while (true) {
+					Data* pop = nullptr;
+					pop - (Data*)PopEntrySList(GHeader);
+
+					if (pop) {
+						cout << pop->_rand << endl;
+						delete pop;
+					}
+					else {
+						cout << "NONE" << endl;
+					}
+				}
+			});
 	}
 
 	GThreadManager->Join();
--- GameServer/LockFreeStack.h
+++ GameServer/LockFreeStack.h
@@ -3,7 +3,7 @@
 //-------------------
 //		1차시도
 //-------------------
-
+/*
 struct SListEntry 
 {
 	SListEntry* next;
@@ -22,20 +22,70 @@ struct SListHeader {
 	SListEntry* next = nullptr;
 };
 
-void InitializedHead(SListHeader* header) {
-	header->next = nullptr;
-}
+void InitializeHead(SListHeader* header);
+void PushEntrySList(SListHeader* header, SListEntry* entry);
+SListEntry* PopEntrySList(SListHeader* header);
+*/
 
-void PushEntrySList(SListHeader* header, SListEntry* entry) {
-	entry->next = header->next;
-	header->next = entry;
-}
+//-------------------
+//		2차시도
+//-------------------
 
-SListEntry* PopEntrySList(SListHeader* header) {
-	SListEntry* first = header->next;
-	
-	if (first != nullptr)
-		header->next = first->next;
+/*
+struct SListEntry
+{
+	SListEntry* next;
+};
 
-	return first;
-}
+class Data
+{
+public:
+	SListEntry _entry;
+
+	int32 _hp;
+	int32 _mp;
+};
+
+struct SListHeader {
+	SListEntry* next = nullptr;
+};
+
+void InitializeHead(SListHeader* header);
+void PushEntrySList(SListHeader* header, SListEntry* entry);
+SListEntry* PopEntrySList(SListHeader* header);
+*/
+
+//-------------------
+//		3차시도
+//-------------------
+
+DECLSPEC_ALIGN(16)
+struct SListEntry
+{
+	SListEntry* next;
+};
+
+DECLSPEC_ALIGN(16)
+struct SListHeader {
+	SListHeader() {
+		alignment = 0;
+		region = 0;
+	}
+
+	union {
+		struct {
+			uint64 alignment;
+			uint64 region;
+		} DUMMYSTRUCTNAME;
+		struct {
+			uint64 depth : 16;
+			uint64 sequence : 48;
+			uint64 reserved : 4;
+			uint64 next : 60;
+		} HeaderX64;
+	};
+};
+
+void InitializeHead(SListHeader* header);
+void PushEntrySList(SListHeader* header, SListEntry* entry);
+SListEntry* PopEntrySList(SListHeader* header);
